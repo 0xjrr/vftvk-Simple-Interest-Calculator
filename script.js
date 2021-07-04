e4b06c2fb4afef9b7cff43b1502616d64c2f9c30
0xjrr <86163146+0xjrr@users.noreply.github.com>
2021-07-04 13:34:00 +0100

    Last commit on script.js

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+//set const values for boxes
+const rateValue = document.getElementById("rate_val")
+const principalField = document.getElementById("principal")
+const rateField = document.getElementById("rate")
+const yearsField = document.getElementById("years")
+const resultField = document.getElementById("result")
+
+// init vars
+let principal
+let rate
+let years
+let interest
+let year
+
+const updateRate = () => {
+    // Update rate in the span elem
+    rate = rateField.value;
+    rateValue.innerText = rate
 }
-        
+
+// Compute value
+function compute() {
+    // get current value 
+    principal = principalField.value;
+    
+    if (principal <= 0) {
+        // alert if principal is less than zero and place cursor in principal elem 
+        alert(`Enter a positive number geater than zero on the Principal Amount`)
+        principalField.focus();
+        principalField.select();
+        resultField.innerText = `Enter a positive number`
+    } else {
+        // Display text with value on result span elem 
+        rate = rateField.value;
+        years = yearsField.value;
+        year = new Date().getFullYear() + parseInt(years);
+        interest = principal * years * rate / 100;
+        resultString = `If you deposit ${principal}
+        at an interest rate of ${rate}%.
+        You will receive an amount of ${interest},
+        in the year ${year}.`
+        resultField.innerText = resultString;
+    }
+
+}
